d6b03afe1ed0543a9ed810a79d1392a09ef68152
Saarang <saaranganand2013@gmail.com>
2023-10-12 18:34:12 -0700

    add tasks to textfile

--- main.py
+++ main.py
@@ -5,27 +5,36 @@ while True:
     match user_action:
         case 'add':
             todo = input("Enter your todo: ") + "\n"
-            file = open('todos.txt', 'r')
+
+            file = open('files/todos.txt', 'r')
             todos = file.readlines()
             file.close()
 
             todos.append(todo)
 
-            file = open('todos.txt', 'w')
+            file = open('files/todos.txt', 'w')
             file.writelines(todos)
             file.close()
+
         case 'show':
+            file = open('files/todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
+
         case 'edit':
             number = int(input("Enter todo number to edit: "))
             number = number; #index
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+
         case 'complete':
             number = int(input("Enter todo number to complete: "))
             todos.pop(number-1)
+
         case 'exit':
             break
 
