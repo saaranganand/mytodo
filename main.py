6cbb962061b75d2032a9aeb8107a805f5d4b679d
Saarang <saaranganand2013@gmail.com>
2023-10-12 19:18:32 -0700

    added clear feature

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = input("Type add, show, edit, complete, clear or exit: ")
     user_action = user_action.strip() # remove trailing and leading spaces
 
     match user_action:
         case 'add':
             todo = input("Enter your todo: ") + "\n"
 
-            file = open('files/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('files/todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
 
         case 'show':
-            file = open('files/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
                 item = item.strip('\n')
@@ -28,13 +25,39 @@ while True:
 
         case 'edit':
             number = int(input("Enter todo number to edit: "))
-            number = number; #index
+            number = number - 1; #index
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + "\n"
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
 
         case 'complete':
             number = int(input("Enter todo number to complete: "))
-            todos.pop(number-1)
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f"{todo_to_remove} completed!"
+            print(message)
+        
+        case 'clear':
+            with open('files/todos.txt', 'w') as file:
+                file.writelines([])
+            
+            message = "All todos cleared!"
+            print(message)
 
         case 'exit':
             break
